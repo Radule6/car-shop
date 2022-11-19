4a6ca45c17fbd20ae64c19cd65cbef6966ed49b8
Marko Radulovic <marko.radulovic04@gmail.com>
2022-11-19 19:49:16 +0100

    navbar fix

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,9 @@
-//TODO 
+//TODO
 //CSS
 //Data validation & sanitization
 //authorization, user/admin
 // sessions
 //site redirect
-
-
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -15,56 +13,48 @@ const API = "https://422backend.cyclic.app";
 const Login = () => {
   const navigate = useNavigate();
 
-//see onSubmit vs formaction for validation/sanitization
+  //see onSubmit vs formaction for validation/sanitization
   const displayLogin = () => (
-      <div>
-        <form onSubmit={verifyLogin}>  
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username"></input>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" is="password"></input>
-            <input type="submit" value="Submit"></input>
-        </form>
-      </div>
+    <div>
+      <form onSubmit={verifyLogin}>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" id="username"></input>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" is="password"></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
   );
 
-  function verifyLogin(event){
-      event.preventDefault();
-  
-    const username = event.target.username.value;
-    const password = event.target.password.value; 
+  function verifyLogin(event) {
+    event.preventDefault();
 
-    const data = {username: username, password: password};
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    const data = { username: username, password: password };
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data),
+    };
 
-    fetch(API + "/checkLogin", options).then(
-      (value) => {
-        value.json().then(
-          (value) => {
-            if(value != false){
-              console.log(value);
-              navigate('/admin');
-            } else {
-              // alert('false');
-            }
-          }
-        )
-      }
-    );
-
-    
+    fetch(API + "/checkLogin", options).then((value) => {
+      value.json().then((value) => {
+        if (value != false) {
+          console.log(value);
+          navigate("/admin");
+        } else {
+          alert("false");
+        }
+      });
+    });
   }
 
-  
   return displayLogin();
-  
 };
 
 export default Login;
